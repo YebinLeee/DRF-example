9586741a813e38536e7019cfdd6700eea3a88d9a
Yebin <lyb2325@gmail.com>
2022-05-07 02:04:51 +0900

    기존 django view-url  방식

--- accountApp/views.py
+++ accountApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+# 기존 장고 방식
+
+def hello_world(request):
+    return HttpResponse('Hello_world!') # 간단한 응답 객체
+
+# DRF 방식
